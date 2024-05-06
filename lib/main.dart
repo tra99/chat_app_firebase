481769d58b652c541a8212268bebef78b6f4aaa2
Hok Sochetra <chetrafc60@gmail.com>
2024-05-07 00:27:57 +0700

    fix error

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,6 @@ import 'package:chat_app_new_version/chatgpt/providers/models_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'chat_classroom/screen/homescreen_chat.dart';
-import 'chat_classroom/shared/constant.dart';
 import 'chat_classroom/splashscreen/splashscreen.dart';
 
 
@@ -15,32 +12,27 @@ void main() async{
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_)=>ModelsProvider()),
-        ChangeNotifierProvider(create: (_)=>ChatProvider())
+        ChangeNotifierProvider(create: (_) => ModelsProvider()),
+        ChangeNotifierProvider(create: (_) => ChatProvider())
       ],
       child: MaterialApp(
         theme: ThemeData(
-          primaryColor: Constants().primaryColor,
-          scaffoldBackgroundColor: Colors.white
+          primaryColor: Colors.blue, // Replace with your desired primary color
+          scaffoldBackgroundColor: Colors.white,
         ),
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
-        home:  HomeScreenChat()
-        
+        home: SplashPage(),
       ),
     );
   }
 }
+
 
